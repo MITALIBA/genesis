ddbed8c4fe5ff9ebc0c00577a18f9303624ac43e
MITALIBA <159750513+MITALIBA@users.noreply.github.com>
2024-07-05 14:22:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+
+
+inquirer
+  .prompt([
+    "enter your url"
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_imgs.png'));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
